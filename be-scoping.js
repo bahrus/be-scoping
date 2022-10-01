2293c2f6404e82a5654e2aa625d19a9a401ca0fc
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-10-01 19:52:48 -0400

    Initial commit

---
+++ be-scoping.js
@@ -0,0 +1,27 @@
+import { define } from 'be-decorated/be-decorated.js';
+import { register } from "be-hive/register.js";
+export class BeScoping {
+    async intro(proxy, self) {
+        const { PropertyBag } = await import('trans-render/lib/PropertyBag.js');
+        const pg = new PropertyBag();
+        proxy.scope = pg.proxy;
+        proxy.resolved = true;
+    }
+}
+const tagName = 'be-scoping';
+const ifWantsToBe = 'scoping';
+const upgrade = 'script';
+define({
+    config: {
+        tagName,
+        propDefaults: {
+            upgrade,
+            ifWantsToBe,
+            intro: 'intro'
+        }
+    },
+    complexPropDefaults: {
+        controller: BeScoping,
+    }
+});
+register(ifWantsToBe, upgrade, tagName);
---
+++ be-scoping.ts
@@ -0,0 +1,34 @@
+import {define, BeDecoratedProps} from 'be-decorated/be-decorated.js';
+import {register} from "be-hive/register.js";
+import {Actions, PP, Proxy} from './types';
+
+export class BeScoping implements Actions{
+    async intro(proxy: Proxy, self: Element){
+        const {PropertyBag} = await import('trans-render/lib/PropertyBag.js');
+        const pg = new PropertyBag();
+        proxy.scope = pg.proxy;
+        proxy.resolved = true;
+    }
+}
+
+const tagName = 'be-scoping';
+
+const ifWantsToBe = 'scoping';
+
+const upgrade = 'script';
+
+define<Proxy & BeDecoratedProps<Proxy, Actions>, Actions>({
+    config:{
+        tagName,
+        propDefaults:{
+            upgrade,
+            ifWantsToBe,
+            intro: 'intro'
+        }
+    },
+    complexPropDefaults:{
+        controller: BeScoping,
+    }
+});
+
+register(ifWantsToBe, upgrade, tagName);
